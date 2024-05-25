0e4cc4ba502215651f714926ba7335ea7a49380e
Aayush Raj <rajaayush248@gmail.com>
2024-05-25 19:08:50 +0530

    DB Connected

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ import { sendToken } from "../utils/jwtToken.js";
 
 export const register = catchAsyncErrors(async (req, res, next) => {
   const { name, email, phone, password, role } = req.body;
+  console.log(req.body);
   if (!name || !email || !phone || !password || !role) {
     return next(new ErrorHandler("Please fill full form!"));
   }
@@ -24,6 +25,7 @@ export const register = catchAsyncErrors(async (req, res, next) => {
 
 export const login = catchAsyncErrors(async (req, res, next) => {
   const { email, password, role } = req.body;
+  console.log(req.body);
   if (!email || !password || !role) {
     return next(new ErrorHandler("Please provide email ,password and role."));
   }
